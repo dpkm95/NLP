0b7ed82a9e2729c06c860ec0168768f59c59a2ff
Gautam B <t-gauba@microsoft.com>
2016-09-03 20:46:10 +0530

    Retaining punctuation in cleaned corpus

--- LUE1/preprocessor/PreProcessor.py
+++ LUE1/preprocessor/PreProcessor.py
@@ -1,15 +1,19 @@
-import json,re,sys
+import json, re, sys
+
 
 class PreProcessor:
     # Performs pre-processing of data
-    def pre_process(data_filename, config_filename, output_filename):
+    def __init__(self):
+        pass
+
+    def pre_process(self, data_filename, config_filename, output_filename):
         with open(data_filename, 'r') as data_file, open(config_filename, 'r') as config_file, \
                 open(output_filename, 'w') as output_filename:
             # Load the regex configuration file
             config = json.load(config_file)
             for line in data_file.readlines():
                 # Try to stringify a hashtag if it's in camel case - e.g. #ModiInDubai => Modi In Dubai
-                line = camel_case_split(line)
+                line = self.camel_case_split(line)
                 # Replace all the hashtags with HASHTAG token
                 for k, v in config['replace_tokens'].items():
                     line = re.sub(v, k, line)
@@ -21,18 +25,16 @@ class PreProcessor:
                     line = re.sub(token, ' ', line)
                 output_filename.write(re.sub(r'(\s+)', ' ', line).strip().lower() + '\n')
 
-
     # Try to stringify a hashtag if it's in camel case - e.g. #ModiInDubai => Modi In Dubai
-    def hashtag_stringify(token):
+    def hashtag_stringify(self, token):
         matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', token)
         words = [m.group(0) for m in matches]
         return ' '.join(words) if len(words) > 1 else '#' + token
 
-
-    def camel_case_split(line):
+    def camel_case_split(self, line):
         phrases = []
         for word in re.split(r'\s+', line):
-            phrases.append(hashtag_stringify(word[1:]) if len(word) > 0 and word[0] == '#' else word)
+            phrases.append(self.hashtag_stringify(word[1:]) if len(word) > 0 and word[0] == '#' else word)
         return ' '.join(phrases)
 
 
