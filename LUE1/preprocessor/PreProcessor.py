9128dc19ad872864785b6e68346dd7cdf749e826
Gautam B <t-gauba@microsoft.com>
2016-09-03 22:11:44 +0530

    Removed duplicates in the cleaned data.

--- LUE1/preprocessor/PreProcessor.py
+++ LUE1/preprocessor/PreProcessor.py
@@ -1,33 +1,44 @@
 import json, re, sys
 
+from OrderedSet import OrderedSet
 
+
+# Performs pre-processing of data
 class PreProcessor:
-    # Performs pre-processing of data
-    def __init__(self):
-        pass
-
-    def pre_process(self, data_filename, config_filename, output_filename):
-        with open(data_filename, 'r') as data_file, open(config_filename, 'r') as config_file, \
-                open(output_filename, 'w') as output_filename:
+    def __init__(self, config_filename):
+        with open(config_filename, 'r') as config_file:
             # Load the regex configuration file
-            config = json.load(config_file)
+            self.config = json.load(config_file)
+
+    def pre_process(self, data_filename):
+        lines = OrderedSet()
+        with open(data_filename, 'r') as data_file:
             for line in data_file.readlines():
                 # Try to stringify a hashtag if it's in camel case - e.g. #ModiInDubai => Modi In Dubai
                 line = self.camel_case_split(line)
                 # Replace all the hashtags with HASHTAG token
-                for k, v in config['replace_tokens'].items():
+                for k, v in self.config['replace_tokens'].items():
                     line = re.sub(v, k, line)
                 # Replacing the emoticons with their title.
-                for title, icons in config['emoticons'].items():
+                for title, icons in self.config['emoticons'].items():
                     for icon in icons:
                         line = line.replace(icon, title)
                 # Remove all the tokens specified in the configuration JSON file.
-                for token in config['remove_tokens']:
+                for token in self.config['remove_tokens']:
                     line = re.sub(token, ' ', line)
-                output_filename.write(re.sub(r'(\s+)', ' ', line).strip().lower() + '\n')
+                line = re.sub(r'(\s+)', ' ', line).strip().lower()
+                lines.add(line)
+        return lines
 
+    @staticmethod
+    def write_file(filename, lines):
+        with open(filename, 'w') as output_file:
+            for line in lines:
+                output_file.write(line + '\n')
+
+    @staticmethod
     # Try to stringify a hashtag if it's in camel case - e.g. #ModiInDubai => Modi In Dubai
-    def hashtag_stringify(self, token):
+    def hashtag_stringify(token):
         matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', token)
         words = [m.group(0) for m in matches]
         return ' '.join(words) if len(words) > 1 else '#' + token
@@ -42,5 +53,5 @@ class PreProcessor:
 # Arguments : Path to input file, Path to config file, Path to output file
 # e.g. python PreProcessor.py tweets.txt preprocess_config.json cleaned_data.txt
 if __name__ == '__main__':
-    p=PreProcessor()
-    p.pre_process(sys.argv[1], sys.argv[2], sys.argv[3])
+    p = PreProcessor(sys.argv[2])
+    p.write_file(sys.argv[3], p.pre_process(sys.argv[1]))
